a4eeb96a860fc9cc3481bd1b81c0ead99a7869e8
Enmanuel Perez <Enmanuel_21_@hotmail.com>
2019-06-09 16:33:17 -0400

    Creacion de los modelos

---
+++ Persistence/StudentDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Persistence
+{
+    public class StudentDbContext : DbContext
+    {
+        public DbSet<Students> Student { get; set; }
+
+        public StudentDbContext(DbContextOptions options)
+            : base(options)
+        { }
+    }
+}
